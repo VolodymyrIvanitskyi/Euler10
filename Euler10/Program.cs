d15e38da667d5059d841364536e6ed09ce40474e
Vova_Ivanitskyi <vovanhuk2003@gmail.com>
2020-06-23 20:47:19 +0300

    Добавьте файлы проекта.

---
+++ Euler10/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Euler10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
