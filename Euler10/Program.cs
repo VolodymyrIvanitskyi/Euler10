12f4bada7e36623c5801700163c3eed93e941cd7
Vova_Ivanitskyi <vovanhuk2003@gmail.com>
2020-06-23 21:02:10 +0300

    Euler 10 is completed

--- Euler10/Program.cs
+++ Euler10/Program.cs
@@ -6,7 +6,32 @@ namespace Euler10
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int k = 2;
+            int sum = 0;
+            bool flag = true;
+            while(flag)
+            {
+                if(isSimple(k))
+                {
+                    if (k < 200000)
+                    {
+                        sum += k;
+                    }
+                    else flag = false; 
+                }
+                ++k;
+            }
+            Console.WriteLine($"Sum = {sum}");
+        }
+
+        static bool isSimple(int number)
+        {
+            for(int i = 2; i < number/2; i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+            return true;
         }
     }
 }
